9502c5fe2add58cb9e0e6f2b43560cb132da7094
RikoAppDev <riko.appdev@gmail.com>
2023-03-31 16:47:12 +0200

    console input change

--- src/main/java/dev/riko/golftourplanner/Main.java
+++ src/main/java/dev/riko/golftourplanner/Main.java
@@ -6,11 +6,16 @@ import dev.riko.golftourplanner.utils.GenerateData;
 import java.util.List;
 import java.util.Scanner;
 
+import static dev.riko.golftourplanner.facility.FacilityType.GOLF_COURSE;
+
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Amount of places >> ");
-        int amount = scanner.nextInt();
+        int amount;
+        do {
+            System.out.print("Amount of places (min 2) >> ");
+            amount = scanner.nextInt();
+        } while (amount < 2);
 
         GenerateData generateData = new GenerateData(amount);
         List<Place> placeList = generateData.getData();
@@ -22,21 +27,51 @@ public class Main {
         }
         System.out.println();
 
-        System.out.print("Select the starting destination (1 - " + amount + "): ");
-        Place startPlace = placeList.get(scanner.nextInt() - 1);
-        System.out.print("Select the final destination (1 - " + amount + "): ");
-        Place finalPlace = placeList.get(scanner.nextInt() - 1);
-        System.out.print("How much you want to exploring (0 - 10): ");
-        int exploringRate = scanner.nextInt();
+        boolean run = true;
+        while (run) {
+            System.out.print("Place info -> pi | Distance calculator -> dc | EXIT -> e >> ");
+            String type = scanner.next();
+            switch (type) {
+                case "pi" -> {
+                    System.out.print("Search specific city: ");
+                    String city = scanner.next();
 
-        System.out.println();
-        System.out.println("Your starting place is:");
-        startPlace.printPlace();
-        System.out.println("Your final destination is:");
-        finalPlace.printPlace();
-        System.out.println();
+                    for (Place place : placeList) {
+                        if (place.getTitle().equalsIgnoreCase(city)) {
+                            place.printPlace();
+                        }
+                    }
+                }
+                case "lc" -> {
+                    System.out.print("Select the starting destination (1 - " + amount + "): ");
+                    Place startPlace = placeList.get(scanner.nextInt() - 1);
+                    Place finalPlace;
+                    String notGc = "n";
 
-        SearchOptimalTrip optimalTrip = new SearchOptimalTrip(startPlace, finalPlace, exploringRate);
-        System.out.println("The air distance from " + startPlace.getTitle() + " to " + finalPlace.getTitle() + " is " + String.format("%.2f", optimalTrip.getOptimalTripLength()) + "km.\n");
+                    do {
+                        System.out.print("Select the final destination (1 - " + amount + "): ");
+                        finalPlace = placeList.get(scanner.nextInt() - 1);
+                        if (!finalPlace.getFacilityList().contains(GOLF_COURSE)) {
+                            System.out.println("Your final destination does not have golf course!");
+                            System.out.print("Would you like to select again? | y, n >> ");
+                            notGc = scanner.next();
+                        }
+                    } while (notGc.equals("y") && !finalPlace.getFacilityList().contains(GOLF_COURSE));
+                    System.out.print("How much you want to exploring (0 - 10): ");
+                    int exploringRate = scanner.nextInt();
+
+                    System.out.println();
+                    System.out.println("Your starting place is:");
+                    startPlace.printPlace();
+                    System.out.println("Your final destination is:");
+                    finalPlace.printPlace();
+                    System.out.println();
+
+                    SearchOptimalTrip optimalTrip = new SearchOptimalTrip(startPlace, finalPlace, exploringRate);
+                    System.out.println("The air distance from " + startPlace.getTitle() + " to " + finalPlace.getTitle() + " is " + String.format("%.2f", optimalTrip.getOptimalTripLength()) + "km.\n");
+                }
+                default -> run = false;
+            }
+        }
     }
 }
